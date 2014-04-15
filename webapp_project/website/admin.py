0795625dad439d94762c63464ac41acf12723633
Rana Singh <rana@n0de.net>
2014-04-15 21:33:49 +0000

    added main tables

--- webapp_project/website/admin.py
+++ webapp_project/website/admin.py
@@ -4,9 +4,29 @@ from .models import Genre
 from .models import Tag
 from .models import Book
 from .models import Asset
+from .models import Author
 
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
 
-admin.site.register(Genre)
-admin.site.register(Tag)
-admin.site.register(Book)
-admin.site.register(Asset)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ['name']
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('text', )
+    search_fields = ['text']
+
+class AssetAdmin(admin.ModelAdmin):
+    list_display = ('book', 'asset_type')
+    search_fields = ['book']
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ['name']
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Asset, AssetAdmin)
+admin.site.register(Author, AuthorAdmin)
